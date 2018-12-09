f2c0b7426d9e457909f1efd197b4b6a2fe55587d
george <you@example.com>
2018-12-10 00:10:08 +0200

    third

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from subprocess import Popen
 
 
 if __name__ == "__main__":
-    #Popen(["python", "instabot.py/example.py"])
+    Popen(["python", "instabot.py/example.py"])
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
     from django.core.management import execute_from_command_line
